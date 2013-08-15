f660c38f74e79f6eb3b6410ed81bddcb3c1ddea0
Jimmy Joe <takeonechief@gmail.com>
2013-08-14 17:14:01 -0700

    update
    update

---
+++ portfolio-test.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+
+		<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
+		Remove this if you use the .htaccess -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+		<title>My Portfolio</title>
+		<meta name="description" content="" />
+		<meta name="author" content="Jimmy Joe" />
+
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+
+		<link rel="stylesheet" href="/css/reset.css" type="text/css">
+		<link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
+		<link rel="stylesheet" href="/css/bootstrap-responsive.css" type="text/css">
+		<link rel="stylesheet" href="/css/animate.css" type="text/css">
+		<link rel="stylesheet" href="/css/slider.css" type="text/css">
+		<link rel="stylesheet" href="/css/bootstrap-switch.css" type="text/css">
+		<link rel="stylesheet" href="/css/bootstrap-colorpicker.css" type="text/css">
+
+		<script src="/js/jquery-1.8.2.min.js"></script>
+		<script src="/js/bootstrap.js" type="text/javascript"></script>
+		<script src="/js/bootstrap-slider.js"></script>
+		<script src="/js/bootstrap-switch.js"></script>
+		<script src="/js/bootstrap-colorpicker.js"></script>
+		<script src="/js/angular.js"></script>
+		<link rel="stylesheet/less" href="/exe/portfolio.less?v=1.1"/>
+		<script src="/js/less-1.3.0.min.js"></script>
+		<script src="/js/portfolio.js"></script>
+
+		<link href='http://fonts.googleapis.com/css?family=Engagement' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Montserrat:700,400' rel='stylesheet' type='text/css'>
+		<!-- Replace favicon.ico & apple-touch-icon.png in the root of your domain and delete these references -->
+		<link rel="shortcut icon" href="/favicon.ico" />
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+	</head>
+
+	<body>
+		<?php include_once("ga_tracking.php")?>
+		<div class="p">
+			<div id="fixedHeader">
+				<ul>
+					<li>
+						<a href="http://goo.gl/DRXrK" target="_blank"><img src="/img/portfolio/32_facebook.png"/> &nbsp;Friend me</a>
+					</li>
+					<li>
+						<a href="http://goo.gl/RZLd1" target="_blank"><img src="/img/portfolio/32_linkedin.png"/> &nbsp;Connect with me</a>
+					</li>
+					<li><img src="/img/portfolio/32_skype.png"/> &nbsp;Skype: <small>takeonechief</small>
+					</li>
+					<li>
+						<a href="mailto:takeonechief@gmail.com?subject=We're Interested In You&body=Hi, We would love to chat with you. Please get back to us with date and time that's convenient."><img src="/img/portfolio/32_mail.png" target="_blank"/> &nbsp;Email Me</a>
+					</li>
+					<li>
+						<a href="http://goo.gl/rcsre"  target="_blank"><img src="/img/portfolio/32_video.png"/> &nbsp;getClever: <small>#JimmyJoe</small></a>
+					</li>
+					<li><img src="/img/portfolio/32_phone.png"/> &nbsp;<small>310.622.2910</small>
+					</li>
+				</ul>
+			</div>
+			<div style="width:100%;height:1600px">d</div>
+			
+		</div>
+	</body>
+</html>
